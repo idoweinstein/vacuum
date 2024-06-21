6eef8489db27d383d0edfe1e62a8469391ff0c92
Amit Gabay <amitgabay9142@gmail.com>
2024-06-21 19:08:27 +0300

    Fixed casting to abstract classes

--- src/robot.cc
+++ src/robot.cc
@@ -7,7 +7,7 @@ Robot::Robot(unsigned int max_robot_steps,
              std::pair<unsigned int, unsigned int>& docking_station_position) : max_robot_steps(max_robot_steps),
                                                                                 battery_controller(max_battery_steps),
                                                                                 location_manager(wall_map, dirt_map, docking_station_position),
-                                                                                navigation_system((BatterySensor)battery_controller,
-                                                                                                  (DirtSensor)location_manager,
-                                                                                                  (WallSensor)location_manager)
+                                                                                navigation_system((BatterySensor&)battery_controller,
+                                                                                                  (DirtSensor&)location_manager,
+                                                                                                  (WallSensor&)location_manager)
 { }
