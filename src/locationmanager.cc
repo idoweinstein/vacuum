043733bd23d1ca02c217d78d9e0fded0a7e87a2c
Ido Weinstein <idoweinstein@mail.tau.ac.il>
2024-06-25 06:56:57 +0300

    Fixed CR notes

--- src/locationmanager.cc
+++ src/locationmanager.cc
@@ -6,10 +6,17 @@
 LocationManager::LocationManager(std::vector<std::vector<bool>>& wall_map, std::vector<std::vector<unsigned int>>& dirt_map, UPosition docking_station_position) :
     wall_map(wall_map), dirt_map(dirt_map), current_position(docking_station_position), docking_station_position(docking_station_position), total_dirt_count(0)
 {
-    /* Calculate total dirt count */
-    for (auto row : dirt_map)
+
+    for (std::vector<unsigned int> row : dirt_map)
     {
-        for (auto dirt : row)
+        /* Validate map shape */
+        if (row.size() != dirt_map[0].size())
+        {
+            throw std::invalid_argument("Dirt map is not a rectangle");
+        }
+
+        /* Calculate total dirt count */
+        for (unsigned int dirt : row)
         {
             total_dirt_count += dirt;
         }
@@ -34,9 +41,18 @@ void LocationManager::cleanCurrentPoisition()
 
 bool LocationManager::isWall(Direction direction) const
 {
-    UPosition next_position = UPosition::computePosition(current_position, direction);
+    if ((current_position.first == 0 &&  direction == Direction::NORTH)
+        || (current_position.second == 0 &&  direction == Direction::WEST)
+        || (current_position.first == wall_map.size() - 1 &&  direction == Direction::SOUTH)
+        || (current_position.second == wall_map[0].size() - 1 &&  direction == Direction::EAST))
+    {
+        /* Off-grid positions are considered a wall */
+        return true;
+    }
 
-    return wall_map[next_position.first][next_position.second];
+    UPosition suggested_position = UPosition::computePosition(current_position, direction);
+
+    return wall_map[suggested_position.first][suggested_position.second];
 }
 
 void LocationManager::move(Direction direction)
