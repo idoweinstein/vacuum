9e0d5ee436e06370068069a5cf915486b4867507
Amit Gabay <amitgabay9142@gmail.com>
2024-06-25 17:48:17 +0300

    Robot Constructor Refactoring

--- src/robot.cc
+++ src/robot.cc
@@ -4,10 +4,12 @@ Robot::Robot(unsigned int max_robot_steps,
              unsigned int max_battery_steps,
              std::vector<std::vector<bool>>& wall_map,
              std::vector<std::vector<unsigned int>>& dirt_map,
-             std::pair<unsigned int, unsigned int>& docking_station_position) : max_robot_steps(max_robot_steps),
-                                                                                battery_controller(max_battery_steps),
-                                                                                location_manager(wall_map, dirt_map, docking_station_position),
-                                                                                navigation_system((BatterySensor&)battery_controller,
-                                                                                                  (DirtSensor&)location_manager,
-                                                                                                  (WallSensor&)location_manager)
+             UPoint& docking_station_position)
+    : max_robot_steps(max_robot_steps),
+      battery_controller(max_battery_steps),
+      location_manager(wall_map, dirt_map, docking_station_position),
+      navigation_system(
+            (BatterySensor&) battery_controller,
+            (DirtSensor&) location_manager,
+            (WallSensor&) location_manager)
 { }
