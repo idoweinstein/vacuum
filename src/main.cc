fd83051336756863d623ad02ee201a3902e7cb74
Amit Gabay <amitgabay9142@gmail.com>
2024-06-28 00:41:50 +0300

    First commit

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,29 @@
+#include <exception>
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "robot.h"
+#include "robotlogger.h"
+#include "robotdeserializer.h"
+
+int main(int argc, char* argv[])
+{
+    if (2 == argc)
+    {
+        std::vector<std::vector<bool>> wall_map;
+        std::vector<std::vector<unsigned int>> dirt_map;
+
+        const std::string input_file_name(argv[1]);
+
+        RobotLogger::setLogFile(input_file_name);
+
+        Robot robot = RobotDeserializer::deserializeFromFile(wall_map, dirt_map, input_file_name);
+
+        robot.run();
+    }
+
+    else
+    {
+        RobotLogger::logError("Invalid number of arguments!\nUsage: myrobot <input_file>");
+    }
 }
