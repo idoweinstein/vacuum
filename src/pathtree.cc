4254fe4c3e7b61fe52682f9069c6a2b380d86c06
Amit Gabay <amitgabay9142@gmail.com>
2024-06-28 16:31:01 +0300

    PathTree Initialize nodes in-place optimization

--- src/pathtree.cc
+++ src/pathtree.cc
@@ -2,24 +2,22 @@
 
 unsigned int PathTree::insertRoot(Position position)
 {
-    PathNode root_node = {
+    node_pool.emplace_back({
         kNoParent,          // PathNode.parent_index
         Direction::STAY,    // PathNode.direction (doesn't matter - it's the first path node)
         position            // PathNode.position
-    };
-    node_pool.push_back(root_node);
+    });
 
-    return 0; // Root Index
+    return 0; // Root Node Index
 }
 
 unsigned int PathTree::insertChild(unsigned int parent_index, Direction direction_to_child, Position child_position)
 {
-    PathNode child_node = {
+    node_pool.emplace_back({
         (int)parent_index,  // PathNode.parent_index
         direction_to_child, // PathNode.direction
         child_position      // PathNode.position
-    };
-    node_pool.push_back(child_node);
+    });
 
     unsigned int child_index = node_pool.size() - 1;
     return child_index;
