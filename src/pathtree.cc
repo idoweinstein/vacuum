1f6df1282c60ec78f4aae6c1001ba9f1458b88b3
Amit Gabay <amitgabay9142@gmail.com>
2024-06-28 16:52:26 +0300

    Fixed compilation errors

--- src/pathtree.cc
+++ src/pathtree.cc
@@ -2,22 +2,22 @@
 
 unsigned int PathTree::insertRoot(Position position)
 {
-    node_pool.emplace_back({
+    node_pool.emplace_back(
         kNoParent,          // PathNode.parent_index
         Direction::STAY,    // PathNode.direction (doesn't matter - it's the first path node)
         position            // PathNode.position
-    });
+    );
 
     return 0; // Root Node Index
 }
 
 unsigned int PathTree::insertChild(unsigned int parent_index, Direction direction_to_child, Position child_position)
 {
-    node_pool.emplace_back({
+    node_pool.emplace_back(
         (int)parent_index,  // PathNode.parent_index
         direction_to_child, // PathNode.direction
         child_position      // PathNode.position
-    });
+    );
 
     unsigned int child_index = node_pool.size() - 1;
     return child_index;
