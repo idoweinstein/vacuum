7539dc5455a084512df9900d01df5847c4cfb737
Amit Gabay <amitgabay9142@gmail.com>
2024-07-03 15:44:37 +0300

    Rafactored Logger

--- src/logger.cc
+++ src/logger.cc
@@ -2,7 +2,7 @@
 
 Logger::~Logger()
 {
-    for (std::ofstream& file: log_files)
+    for (std::ofstream& file : log_files)
     {
         file.close();
     }
@@ -10,15 +10,16 @@ Logger::~Logger()
 
 void Logger::logMessage(LogLevel log_level, LogOutput output, const std::string& message)
 {
-    const std::string log_prefix = log_level_prefix.at(log_level);
+    const std::string& log_prefix = log_level_prefix.at(log_level);
 
     if (LogOutput::FILE == output)
     {
-        for (std::ofstream& file: log_files)
+        for (std::ofstream& file : log_files)
         {
             file << log_prefix << message << std::endl;
         }
     }
+
     else if (LogOutput::CONSOLE == output)
     {
         std::cout << log_prefix << message << std::endl;
