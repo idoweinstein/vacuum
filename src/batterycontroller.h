534ba1d5aa4b906e44c6b0e3fc39b37014714e63
Amit Gabay <amitgabay9142@gmail.com>
2024-07-03 14:15:08 +0300

    Eliminated Magic Numbers

--- src/batterycontroller.h
+++ src/batterycontroller.h
@@ -8,27 +8,36 @@
 
 class BatteryController : public BatterySensor
 {
+    static constexpr const float kStepsToFullAmount = 20.0f;
+    static constexpr const float kDischargeUnit = 1.0f;
+
     const unsigned int full_amount;
     float current_amount;
 
 public:
     BatteryController(unsigned int full_amount) : full_amount(full_amount), current_amount(full_amount) {}
+
     virtual float getCurrentAmount() const
     {
         return current_amount;
     }
+
     virtual void charge()
     {
-        current_amount = std::min(current_amount + full_amount / (float)20, (float)full_amount);
+        float updated_amount = current_amount + (float)full_amount / kStepsToFullAmount;
+        current_amount = std::min(updated_amount, (float)full_amount);
     }
+
     virtual void discharge()
     {
-        if (current_amount - 1 < 0)
+        float updated_amount = current_amount - kDischargeUnit;
+
+        if (updated_amount < 0)
         {
             throw std::range_error("Empty battery");
         }
 
-        current_amount -= 1;
+        current_amount = updated_amount;
     }
 };
 
